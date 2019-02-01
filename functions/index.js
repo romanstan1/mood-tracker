8e33bdaa3308ef13ab6e924aecd6f981691ade62
Stuart Jones <stuartjones@Armadillo.local>
2019-02-01 11:50:07 +0000

    Workin gon mock

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+// Read teh data from the firestore
+// Display as JSON so backend can use it
+
+var data = require('./mock.json');
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send(data);
+});
