2d9a1bdd286510a8e735cd8b29efb3fea99cab4a
Roman Stankiewicz <roman.stan3@gmail.com>
2019-02-01 15:45:29 +0000

    cleanup

--- src/modules/apis.js
+++ src/modules/apis.js
@@ -1,27 +1,6 @@
 import {messaging} from 'firebase/initialize'
 
-export function registerDeviceForNotifications() {
-  if(!!messaging) {
-    messaging.requestPermission()
-    .then(() => messaging.getToken())
-    .then(token => {
-      let url = 'https://us-central1-room-ipro.cloudfunctions.net/app/registerDevice'
-      fetch(url,
-        {
-          method: "POST",
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body:"token=" + token + "&topic=roomipro1" // the topic name
-        })
-        .then(res => res.json())
-        .then(resp => {})
-        .catch(error => console.log("Error with notification registration", error))
-    })
-    .catch(error => console.log("Error with messaging request persmission", error))
-  }
-}
+
 
 export function postNotificationApi(url, idToken, {title, body, link }) {
   return fetch(url,
